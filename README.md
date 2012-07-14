44df4d2d8d2e2e2bf151312a657f7209c77b3f7c
Anderson Silva <afsilva@gmail.com>
2012-07-14 11:12:54 -0400

    v1.1.2 branch: from ListActivity to Activity

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ my personal playground for android programming
 
 It is my attempt to provide useful information about Red Hat's Openshift with this app. Yet this  project is not associated with Red Hat or
 Openshift at all.
+
+v1.1.2 branch
+
+- I have modified the backend of this application so it extends an Activity, isntead of a ListActivity. The idea is to start adding new more resources in the app and eventually changing the 'text' based status of Openshift to an image.
+- I have also created a new layout file to control the font format of the ListView.
--- src/com/andersonsilva/cel/MainActivity.java
+++ src/com/andersonsilva/cel/MainActivity.java
@@ -5,20 +5,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.net.ssl.HttpsURLConnection;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.app.ListActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
 	ArrayAdapter<String> adapter;
 
@@ -44,6 +46,9 @@ public class MainActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final ListView lv = (ListView) findViewById(android.R.id.list);
 
 		// The next two lines are used to I can make the https call on
 		// checkCloudLight on the main application thread. Not the best
@@ -56,10 +61,10 @@ public class MainActivity extends ListActivity {
 		// Here's where we get the current status of our Cloud service
 		checkCloudLight();
 
-		adapter = new ArrayAdapter<String>(this, R.layout.activity_main, items);
-		this.setListAdapter(adapter);
+		adapter = new ArrayAdapter<String>(this, R.layout.activity_main_list,
+				items);
+		lv.setAdapter(adapter);
 
-		lv = getListView();
 		setListViewBackgroundColor(lv, items[0]);
 		lv.setOnItemClickListener(new OnItemClickListener() {
 
